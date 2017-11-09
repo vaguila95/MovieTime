08eafe5eb2876be6557a72726b76af5db49dec90
vaguila <vlaguila@uc.cl>
2017-11-09 18:35:37 -0300

    Scraping inicial

---
+++ Scraping.py
@@ -0,0 +1,13 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+# películas CinMark
+url = 'https://www.cinemark.cl/movies'
+client = uReq(url)
+html = client.read()
+client.close()
+mark_soup = soup(html, "html.parser")
+peliculas = mark_soup.findAll('li', {'class': 'item movie-list-li box'})
+for pelicula in peliculas:
+    titulo = pelicula.div.h4.a.text
+    print(titulo)
