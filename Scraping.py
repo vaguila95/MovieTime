5979a63dd9dcd743ae5254b3efb7c2e703aac0ab
vaguila <vlaguila@uc.cl>
2017-11-11 19:06:56 -0300

    Esquema listo

--- Scraping.py
+++ Scraping.py
@@ -8,75 +8,53 @@ def souping(url):
     client.close()
     return soup(html, "html.parser")
 
-ret = dict()
+collection = list()
 
 
 # CineMark
 url = 'https://www.cinemark.cl/movies'
-ret['cinemark'] = dict()
-
+cinemark = dict()
+cinemark['name'] = 'cinemark'
+cinemark['movies'] = list()
 mark_soup = souping(url)
 peliculas = mark_soup.findAll('li', {'class': 'item movie-list-li box'})
-# cines = list()
-# for o in souping(peliculas[0].div.a['href']).findAll('select', {
-#         'class': 'movie-showtimes-selector'
-#     })[0].findAll('option'):
-#     if o['value'] != '':
-#         cines.append(o['value'])
+
 for pelicula_html in peliculas:
-    cinemark_dic['name'] = pelicula_html.div.h4.a.text.lower()
-    cines = dict()
+    pelicula = dict()
+    pelicula['name'] = pelicula_html.div.h4.a.text.lower()
+    print(pelicula['name'])
+    pelicula['theaters'] = list()
 
     url_pelicula = pelicula_html.div.a['href']
     pelicula_soup = souping(url_pelicula)
     for option in pelicula_soup.findAll('select', {
         'class': 'movie-showtimes-selector'
     })[0].findAll('option'):
-        cine_key = option['value']
-        cine_value = dict()
+        cine = dict()
+        cine['name'] = option['value']
+        cine['rooms'] = list()
+        print(pelicula_soup.findAll('ul', {
+            'class': 'movie-showtime-panel ' + cine['name']
+        }))
 
-        for ul in pelicula_soup.findAll('ul', {
-            'class': 'movie-showtime-panel ' + cine_key
-        }):
-            salas_value = dict()
+    #     for li in pelicula_soup.findAll('ul', {
+    #         'class': 'movie-showtime-panel ' + cine['name']
+    #     })[0].findAll('li'):
+    #         if li['class'] != 'showtime-detail':
+    #             sala = dict()
+    #             sala['name'] = li.text.strip(':')
+    #             sala['days'] = list()
+    #             print(sala['name'])
+    #         else:
+    #             dia = dict()
+    #             dia['day'] = li.findAll(
+    #                 'span', {'class': 'showtime-day'}
+    #             )[0].text
+    #             dia['schedule'] = list()
+    #             for span in li.findAll('span', {'class': 'showtime-hour'}):
+    #                 dia['schedule'].append(span.text)
+    #             cine['rooms'].append(sala)
+    #     pelicula['theaters'].append(cine)
+    # cinemark['movies'].append(pelicula)
 
-            for li in ul.findAll('li'):
-                if li['class'] == 'showtime-detail':
-                    pass
-                else:
-                    salas_key =
-
-    # Opción 2
-    # url = 'https://www.cinemark.cl/movies'
-    # ret['empresa'] = dict()
-    #
-    # mark_soup = souping(url)
-    # peliculas = mark_soup.findAll('li', {'class': 'item movie-list-li box'})
-    # # cines = list()
-    # # for o in souping(peliculas[0].div.a['href']).findAll('select', {
-    # #         'class': 'movie-showtimes-selector'
-    # #     })[0].findAll('option'):
-    # #     if o['value'] != '':
-    # #         cines.append(o['value'])
-    # for pelicula_html in peliculas:
-    #     cinemark_dic['name'] = pelicula_html.div.h4.a.text.lower()
-    #     cines = dict()
-    #
-    #     url_pelicula = pelicula_html.div.a['href']
-    #     pelicula_soup = souping(url_pelicula)
-    #     for option in pelicula_soup.findAll('select', {
-    #         'class': 'movie-showtimes-selector'
-    #     })[0].findAll('option'):
-    #         cine_key = option['value']
-    #         cine_value = dict()
-    #
-    #         for ul in pelicula_soup.findAll('ul', {
-    #             'class': 'movie-showtime-panel ' + cine_key
-    #         }):
-    #             salas_value = dict()
-    #
-    #             for li in ul.findAll('li'):
-    #                 if li['class'] == 'showtime-detail':
-    #                     pass
-    #                 else:
-    #                     salas_key =
+collection.append(cinemark)
