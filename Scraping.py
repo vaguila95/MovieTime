7940f3873cfe49e026126f6df35645b81e5d3544
vaguila <vlaguila@uc.cl>
2017-11-11 14:24:39 -0300

    Estructura JSON

--- Scraping.py
+++ Scraping.py
@@ -1,13 +1,82 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
-# películas CineMark
+
+def souping(url):
+    client = uReq(url)
+    html = client.read()
+    client.close()
+    return soup(html, "html.parser")
+
+ret = dict()
+
+
+# CineMark
 url = 'https://www.cinemark.cl/movies'
-client = uReq(url)
-html = client.read()
-client.close()
-mark_soup = soup(html, "html.parser")
+ret['cinemark'] = dict()
+
+mark_soup = souping(url)
 peliculas = mark_soup.findAll('li', {'class': 'item movie-list-li box'})
-for pelicula in peliculas:
-    titulo = pelicula.div.h4.a.text
-    print(titulo)
+# cines = list()
+# for o in souping(peliculas[0].div.a['href']).findAll('select', {
+#         'class': 'movie-showtimes-selector'
+#     })[0].findAll('option'):
+#     if o['value'] != '':
+#         cines.append(o['value'])
+for pelicula_html in peliculas:
+    cinemark_dic['name'] = pelicula_html.div.h4.a.text.lower()
+    cines = dict()
+
+    url_pelicula = pelicula_html.div.a['href']
+    pelicula_soup = souping(url_pelicula)
+    for option in pelicula_soup.findAll('select', {
+        'class': 'movie-showtimes-selector'
+    })[0].findAll('option'):
+        cine_key = option['value']
+        cine_value = dict()
+
+        for ul in pelicula_soup.findAll('ul', {
+            'class': 'movie-showtime-panel ' + cine_key
+        }):
+            salas_value = dict()
+
+            for li in ul.findAll('li'):
+                if li['class'] == 'showtime-detail':
+                    pass
+                else:
+                    salas_key =
+
+    # Opción 2
+    # url = 'https://www.cinemark.cl/movies'
+    # ret['empresa'] = dict()
+    #
+    # mark_soup = souping(url)
+    # peliculas = mark_soup.findAll('li', {'class': 'item movie-list-li box'})
+    # # cines = list()
+    # # for o in souping(peliculas[0].div.a['href']).findAll('select', {
+    # #         'class': 'movie-showtimes-selector'
+    # #     })[0].findAll('option'):
+    # #     if o['value'] != '':
+    # #         cines.append(o['value'])
+    # for pelicula_html in peliculas:
+    #     cinemark_dic['name'] = pelicula_html.div.h4.a.text.lower()
+    #     cines = dict()
+    #
+    #     url_pelicula = pelicula_html.div.a['href']
+    #     pelicula_soup = souping(url_pelicula)
+    #     for option in pelicula_soup.findAll('select', {
+    #         'class': 'movie-showtimes-selector'
+    #     })[0].findAll('option'):
+    #         cine_key = option['value']
+    #         cine_value = dict()
+    #
+    #         for ul in pelicula_soup.findAll('ul', {
+    #             'class': 'movie-showtime-panel ' + cine_key
+    #         }):
+    #             salas_value = dict()
+    #
+    #             for li in ul.findAll('li'):
+    #                 if li['class'] == 'showtime-detail':
+    #                     pass
+    #                 else:
+    #                     salas_key =
---
+++ pruebas.py
@@ -0,0 +1,2 @@
+s = 'asesinato en el oriente express'
+print(s.replace(' ', '-'))
